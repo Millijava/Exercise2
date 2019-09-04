e1f164f30f92b16cea77d3fa1283f89b5864c73b
GBTC441003UR <GBTC441003UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-09-04 16:43:59 -0400

    Exercie2

---
+++ src/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("Today is August 24, 2019. I am alive and happy!");
+    }
+}
+
